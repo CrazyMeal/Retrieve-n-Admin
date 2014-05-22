f2ad856fada5994d54285882e6c6562c97eee064
KVN <kvndesign.mail@gmail.com>
2014-05-22 16:53:00 +0200

    Integration test for slider

--- js/test/e2e/integration_test.js
+++ js/test/e2e/integration_test.js
@@ -1,15 +1,36 @@
 'use strict';
 
 describe('homepage', function() {
-	var ptor = protractor.getInstance();
-
-	describe('test', function() {
-		//ptor.driver.get('C:/Users/KVN/Documents/Projet2A-2014/JSBase/index.html');
-		//ptor.driver.get('../index.html');
-		//browser.driver.get('C:/Users/KVN/Documents/Projet2A-2014/JSBase/index.html');
-		browser.get('index.html');
-		browser.wait();
-		console.log('le log: ');
-		//expect(ptor.getTitle()).toBe('JSBase');
+	beforeEach(function () {
+    // Load up a view and wait for it to be done with its rendering and epicycles.
+	    browser.get('index.html');
+	    browser.waitForAngular();
 	});
+	
+	it('Title test',
+		function() {
+			expect(browser.getTitle()).toBe('JSBase');
+			console.log(' \n Title test passed');
+		}
+	);
+
+	it('Init test',
+		function() {
+			var elems = element.all(by.repeater('server in dataServer.servers'));
+			expect(elems.count()).toBe(6);
+
+			elems = element.all(by.repeater('chunkServer in splitServers'));
+			expect(elems.count()).toBe(0);
+			console.log('Init test passed');
+		}
+	);
+
+	it('Drag and drop test',
+		function() {
+			element.all(by.css('#serversSlider ul li')).get(0).getText().then(function(dat){
+				console.log(dat);
+				expect(dat).toBe('id: 3\nweight: 107');
+			});
+		}
+	);
 });
